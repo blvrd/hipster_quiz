4c354a5b45f721ca0606a04a4e48275ffc7a888a
Garrett Martin <garrettqmartin8@optimum.net>
2014-01-23 18:31:05 -0600

    change values in score model

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,15 +4,15 @@ class Score < ActiveRecord::Base
 
   
   def refresh_hipster_status
-    if self.value == 10
+    if self.value == 20
       "The Ultimate Hipster. People probably havent heard of you."
-    elsif self.value >= 7 && self.value < 10
+    elsif self.value >= 15 && self.value < 20
       "You are a hipster."
-    elsif self.value >= 5 && self.value < 7
+    elsif self.value >= 10 && self.value < 15
       "You have hipster tendencies, but not enough to move to Brooklyn."
-    elsif self.value >= 2 && self.value < 5
+    elsif self.value >= 4 && self.value < 10
       "Not a hipster."
-    elsif self.value < 2
+    elsif self.value < 4
       "The Anti-Hipster"
     end
   end
